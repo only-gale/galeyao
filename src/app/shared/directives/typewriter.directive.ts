31067b917e6508231b946400e4f1161e16b98fd1
Gale Yao <yaogang1987613@163.com>
2017-01-11 22:03:28 +0800

    Refactory the typewriter directive, it now just can typewrite the objects of type TypewriterContent, and it supports special word and special style for unerasable typing.

--- src/app/shared/directives/typewriter.directive.ts
+++ src/app/shared/directives/typewriter.directive.ts
@@ -1,41 +1,50 @@
 /**
  * Created by gale on 17-1-7.
  */
-import {Directive, ElementRef, Input, AfterViewInit, Output, EventEmitter} from "@angular/core";
+import { Directive, ElementRef, Input, AfterViewInit, Output, EventEmitter } from "@angular/core";
+import { TypewriterContent } from "../po/typewriter.content";
 @Directive({
     selector: '[g-typewriter]'
 })
 
 export class TypewriterDirective implements AfterViewInit {
     ngAfterViewInit(): void {
-        const hostElem = this.el.nativeElement;
-        this._target = hostElem.querySelector('span.wrap');
-        setTimeout(() => this.tick(), this._duration);
+        this._target = this.el.nativeElement.querySelector('span.wrap');
+        setTimeout(() => this.tick(), this._delay);
     }
-    constructor ( private el: ElementRef){}
+
+    constructor( private el: ElementRef ) {
+    }
 
     private _target: Element;
     private _loopNum: number = 0;
     private _index: number = 0;
-    private _duration: number = 1800;
-    private _delay_ratio: number = ( Math.sqrt( 5 ) - 1 ) / 2;
+    private _duration: number = 1500;       // Default duration.
+    private _delay_ratio: number = ( Math.sqrt(5) - 1 ) / 2;
+    private _delay = this._duration * this._delay_ratio;           // Delay before type next sentence out.
+
     private _erasable: boolean = true;
     private _inline: boolean = true;
+    private _decorable: boolean = false;
     private _isDeleting: boolean = false;
-    private _text: string = '';
-    private _text_tmp: string = '';
+
+    private _text_decorated: string = '';
     private _isDone = false;
-    private _delay = this._duration * this._delay_ratio;           // Delay after typed out.
-    private _regex = /[,;]$/;       // Delay if the text match this rule.
+    private _regex = /[,;]$/;       // Delay if the text match this rule. In order to setting delay within a sentence.
+
+    private _cur = -1;
+    private _decorate_templates: string[] = [];
+    private _default_special_class_name = 'special-typewriter';
+
+    private _contents: TypewriterContent[] = [];
+
     @Output() isDone = new EventEmitter<boolean>();
 
-    _contents: Array<string> = [];
-
-    @Input() set contents( contentsArray: Array<string> ) {
+    @Input() set contents( contentsArray: TypewriterContent[] ) {
         this._contents = contentsArray || this._contents;
     }
 
-    @Input() set duration( duration: number) {
+    @Input() set duration( duration: number ) {
         this._duration = duration || this._duration;
     }
 
@@ -44,54 +53,144 @@ export class TypewriterDirective implements AfterViewInit {
     }
 
     @Input() set inline( inline: boolean ) {
-        this._inline = this._erasable || inline;
+        this._inline = inline;
     }
 
-    private tick() {
+    @Input() set decorable( decorable: boolean ) {
+        this._decorable = decorable;
+    }
+
+    private tick(): void {
         let i = this._loopNum % this._contents.length;
-        let fullText = this._contents[ i ];
-        let delta = 150 - Math.random() * 100;
+        let tc = this._contents[ i ];
+        let fullText = tc.getContent();
+
+        if ( i !== this._cur ) {
+            this._cur = i;
+            this._set_decorate_templates(tc);
+        }
 
         if ( this._erasable ) {
-            if ( this._isDeleting ) {
-                this._text = fullText.substring( 0, this._text.length - 1 );
-                delta /= 2;
-                if ( this._text === '' ) {
-                    this._isDeleting = false;
-                    this._loopNum++;
-                    delta = this._delay;
-                }
-            } else {
-                this._text = fullText.substring( 0, this._text.length + 1 );
-                if ( this._text === fullText ) {
-                    delta = this._duration;
-                    this._isDeleting = true;
-                }
-            }
-            this._target.innerHTML = this._text;
+            this._deal_with_erasable(fullText);
         } else {
-            this._text = fullText.substring( 0, this._index++ );
-            if ( !this._inline && this._text === fullText ) {
-                this._loopNum++;
-                this._index = 0;
-                delta = this._duration;
-                if ( this._loopNum < this._contents.length ) {
-                    this._text_tmp += ( this._text + '<br />' );
-                    this._text = '';
-                } else {
-                    this._isDone = true;
-                    this.isDone.emit( this._isDone );
-                }
-            }
-
-            if ( this._text.match( this._regex )) {
-                delta = this._delay;
-            }
-            this._target.innerHTML = this._text_tmp + this._text;
+            this._deal_with_unerasable(fullText);
         }
+    }
+
+    private _deal_with_erasable( fullText: string ): void {
+        let delta = 100 - Math.random() * 50;
+
+        if ( this._isDeleting ) {
+            this._text_decorated = fullText.substring(0, this._text_decorated.length - 1);
+            delta /= 2;     // Speed up.
+            if ( this._text_decorated === '' ) {
+                this._isDeleting = false;
+                this._loopNum++;
+                delta = 0;      // Type next out immediately.
+            }
+        } else {
+            this._text_decorated = fullText.substring(0, this._text_decorated.length + 1);
+            if ( this._text_decorated === fullText ) {
+                delta = this._duration;
+                this._isDeleting = true;
+            }
+        }
+
+        this._show(delta);
+    }
+
+    private _deal_with_unerasable( fullText: string ): void {
+        let delta = 100 - Math.random() * 80;
+
+        let tick_char: string;
+        let origin_text: string;
+        let className: string;
+
+        origin_text = fullText.substring(0, this._index++);
+
+        // Decoration begin
+        tick_char = origin_text.substr(-1);
+        if ( this._decorable ) {
+            className = this._decorate_templates[ Math.max(0, origin_text.length - 1) ];
+            this._text_decorated += (className ? this._render(tick_char, className) : tick_char);
+        } else {
+            this._text_decorated += tick_char;
+        }
+
+        if ( origin_text.match(this._regex) ) {
+            delta = this._delay / 2;
+        }
+
+        // Condition check.
+        if ( !this._inline && origin_text === fullText ) {
+            this._loopNum++;
+            this._index = 0;
+            delta = this._duration;
+            if ( this._loopNum < this._contents.length ) {      // Breaking line.
+                this._text_decorated += '<br />';
+            } else {
+                this._isDone = true;
+                this.isDone.emit(this._isDone);
+            }
+        }
+
+        this._show(delta);
+    }
+
+    private _show( delta: number ): void {
+        this._target.innerHTML = this._text_decorated;
 
         if ( !this._isDone ) {
             setTimeout(() => this.tick(), delta);
+        }
+    }
+
+    private _render( text: string, className: string ): string {
+        return '<span class="' + className + '">' + text + '</span>';
+    }
+
+    private _set_decorate_templates( content: TypewriterContent ): void {
+        let fullText = content.getContent();
+        let specialWords = content.getSpecialWords();
+        let specialClasses = content.getSpecialClasses();
+        let iterable = content.getIterable();
+
+        // Init it.
+        this._decorate_templates = [];
+        this._decorate_templates.length = fullText.length;
+
+        specialWords.forEach(( v, i ) => {
+            let regex: RegExp = this._get_word_regex(v, 'g');
+            if ( iterable ) {
+                while ( regex.exec(fullText) ) {
+                    this._get_it_filled(specialClasses, i, regex.lastIndex, v.length);
+                }
+            } else {
+                regex.exec(fullText);
+                this._get_it_filled(specialClasses, i, regex.lastIndex, v.length);
+            }
+        });
+    }
+
+    private _get_word_regex( v: string, flag?: string ): RegExp {
+        return flag && flag.length > 0 ? new RegExp('\\b' + v + '\\b', flag) : new RegExp('\\b' + v + '\\b');
+    }
+
+    private _get_it_filled( classes: string[], index: number, lastIndex: number, length: number ): void {
+        for ( let x = 1; x <= length; x++ ) {
+            this._decorate_templates[ lastIndex - x ] = this._get_special_class_from(classes, index);
+        }
+    }
+
+    private _get_special_class_from( classes: string[], at: number ): string {
+        if ( classes && classes.length ) {
+            if ( at > classes.length ) {
+                return classes[ 0 ];    // Taking the first one is as default choice.
+            } else {
+                return classes[ at ];
+            }
+        } else {
+            return this._default_special_class_name;
         }
     }
 }
--- src/app/shared/po/typewriter.content.ts
+++ src/app/shared/po/typewriter.content.ts
@@ -2,27 +2,51 @@
  * Created by gale on 17-1-11.
  */
 export class TypewriterContent {
-    constructor( private content: string, private specialWords?: string[], private specialClasses?: string[] ) {
+    constructor( private content: string, private specialWords?: string[], private specialClasses?: string[], private iterable?: boolean ) {
         this._content = content;
-        this._specialWords = specialWords;
-        this._specialClasses = specialClasses;
+        this._specialWords = specialWords || [];
+        this._specialClasses = specialClasses || [];
+        this._iterable = !!iterable;
     }
 
-    private _content: string = '';
-    private _specialWords: string[] = [];
-    private _specialClasses: string[] = [];
+    private _content: string;
+    private _specialWords: string[];
+    private _specialClasses: string[];
+    private _iterable: boolean;
 
-    public setSpecialWord(specialWord: string): void {
+    public getContent(): string {
+        return this._content;
+    }
+    public getSpecialWords(): string[] {
+        return this._specialWords;
+    }
+    public getSpecialClasses(): string[] {
+        return this._specialClasses;
+    }
+    public getIterable(): boolean {
+        return this._iterable;
+    }
+    public setSpecialWord(specialWord: string): TypewriterContent {
         this._specialWords = [ specialWord ];
+        return this;
     }
-    public setSpecialWords(specialWords: string[]): void {
+    public setSpecialWords(specialWords: string[]): TypewriterContent {
         this._specialWords = specialWords;
+        return this;
     }
 
-    public setSpecialClass(specialClass: string): void {
+    public setSpecialClass(specialClass: string): TypewriterContent {
         this._specialClasses = [ specialClass ];
+        return this;
     }
-    public setSpecialClasses(specialClasses: string[]): void {
+    public setSpecialClasses(specialClasses: string[]): TypewriterContent {
         this._specialClasses = specialClasses;
+        return this;
     }
+
+    public setIterable(iterable: boolean): TypewriterContent {
+        this._iterable = iterable;
+        return this;
+    }
+
 }
