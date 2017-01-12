8763ec4c33a1179247d80bc91690ccd479add91c
Gale Yao <yaogang1987613@163.com>
2017-01-12 21:40:34 +0800

    Adjust introduction.

--- src/app/feature/introduction/introduction.component.ts
+++ src/app/feature/introduction/introduction.component.ts
@@ -1,23 +1,24 @@
-import {Component, OnInit, ElementRef, ViewEncapsulation} from '@angular/core';
-import {IntroductionService} from "./introduction.service";
+import { Component, OnInit, ElementRef, ViewEncapsulation } from '@angular/core';
+import { IntroductionService } from "./introduction.service";
+import { TypewriterContent } from "../../shared/po/typewriter.content";
 
 @Component({
-  selector: 'g-introduction',
-  templateUrl: './introduction.component.html',
-  styleUrls: ['./introduction.component.css'],
-  encapsulation: ViewEncapsulation.None,
+    selector: 'g-introduction',
+    templateUrl: './introduction.component.html',
+    styleUrls: [ './introduction.component.css' ],
+    encapsulation: ViewEncapsulation.None,
 })
 export class IntroductionComponent implements OnInit {
 
-  constructor( private el: ElementRef, private introductionService: IntroductionService ) { }
+    constructor( private el: ElementRef, private introductionService: IntroductionService ) { }
 
-  ngOnInit() {
-    const hostElem = this.el.nativeElement;
-    this._target = hostElem.querySelector('span.wrap');
-    this.hobbies = this.introductionService.getHobbies() || this.hobbies;
-  }
+    ngOnInit() {
+        const hostElem = this.el.nativeElement;
+        this._target = hostElem.querySelector('span.wrap');
+        this.hobbies = this.introductionService.getHobbies() || this.hobbies;
+    }
 
-  private hobbies: Array<string> = [];
+    public hobbies: TypewriterContent[] = [];
 
-  private _target: Element;
+    private _target: Element;
 }
--- src/app/feature/introduction/introduction.service.ts
+++ src/app/feature/introduction/introduction.service.ts
@@ -1,16 +1,25 @@
 import { Injectable } from '@angular/core';
+import { TypewriterContent } from "../../shared/po/typewriter.content";
 
 @Injectable()
 export class IntroductionService {
 
-  constructor() { }
+    constructor() { }
 
-  getHobbies() {
-    return [
-      'Type Script.',
-      'Ambient Music.',
-      'Animation.',
-      'Watching movies.'
+    private _default_hobbies = [
+        'Java.',
+        'Type Script.',
+        'Java Script.',
+        'Animation.',
+        'Symphony Music.',
+        'Movies.'
     ];
-  }
+
+    public getHobbies() {
+        return this.formatHobbies(this._default_hobbies);
+    }
+
+    private formatHobbies( hobbies: string[] ): TypewriterContent[] {
+        return hobbies.map(( v: string ) => new TypewriterContent(v));
+    }
 }
